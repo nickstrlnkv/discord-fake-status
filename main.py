855ba45bd77ec7d27aef10f7fb5e04c21b0fd051
nickstrlnkv <nastrelnikov@yandex.ru>
2022-02-03 13:23:29 +0500

    some changes in main.py

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ from time import time
 How to work with bot?
 First of all you need create application on Discord developers.
 Next step choose your created application and write the name 
-that will be displayed in the status
+that will be displayed in the status.
 
 """
 
 RPC = Presence("APPLICATIONS_ID") # Aplication ID you can get on General Information your bot
 """
-# If you want create the buttons on your status, just uncomment this constructions
+# If you want to create the buttons on your status, just uncomment this constructions.
 
 btns = [
     {
@@ -25,11 +25,17 @@ btns = [
 """
 
 RPC.connect()
+
+'''
+About Rich Presence you can read on Discord developers -> Application -> Rich Presence
+There you will see what is responsible for what.
+'''
+
 RPC.update(
-    state="Singleplayer",
-    details="At the: hla_trainstation1",
+    state="", #str state
+    details="", #str details
     start=time(),
-    #buttons=btns,
+    #buttons=btns, #uncomment it if you want to create the buttons on your applications.
     large_image="", #image which you upload on your appliсation, specify the name
     small_image="",
     large_text="", #the text that will be displayed when you hover over the icon
